2e60cc26b09dbea5c5f301661df7c6471320c0f3
Teju0910 <tejasvini_fw15_007@masai.school>
2022-03-02 23:02:41 +0530

    day 1 assi done

---
+++ day 1/1 CALCULATOR/main.js
@@ -0,0 +1,17 @@
+const add = require("./add")
+const subtract = require("./subtract")
+const divide = require("./divide")
+const multiplay = require("./multiplay")
+
+console.log("addition",add(2,2))
+console.log("subtraction" ,subtract(5,2))
+console.log("division",divide(10,5))
+console.log("Multiplication",multiplay(4,4))
+
+const x = 26;
+const y = 2;
+
+console.log("addition",add(x,y))
+console.log("subtraction" ,subtract(x,y))
+console.log("division",divide(x,y))
+console.log("Multiplication",multiplay(x,y))
