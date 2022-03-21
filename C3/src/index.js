35fcd7b4bb2853182320a5b7b4161bb2aa97a3c2
Teju0910 <tejasvini_fw15_007@masai.school>
2022-03-21 11:03:45 +0530

    Update index.js

--- C3/src/index.js
+++ C3/src/index.js
@@ -1,9 +1,15 @@
 
 const express = require("express");
 
-const Usercontroller = require("./controllers/user.controllers")
+const Usercontroller = require("./controllers/user.controllers");
+const Bookcontroller = require("./controllers/book.controllers");
+const Commentcontroller = require("./controllers/comment.controllers");
 
 const app = express();
+
+app.use("/user",Usercontroller);
+app.use("/book",Bookcontroller);
+app.use("/comment",Commentcontroller);
 
 app.use(express.json());
 
