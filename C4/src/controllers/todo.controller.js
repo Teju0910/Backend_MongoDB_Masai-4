686776f3e4a7894ae1ddff610cc07078dbce80b8
Teju0910 <tejasvini_fw15_007@masai.school>
2022-03-28 11:51:27 +0530

    getting conflict again

--- C4/src/controllers/todo.controller.js
+++ C4/src/controllers/todo.controller.js
@@ -2,4 +2,25 @@ const express = require("express");
 
 const router =express.Router();
 
-router.get("")
+const User = require("../modules/user.module")
+
+router.get("", async(req,res) => {
+    try {
+        const users = await User.find().lean().exec();
+    }
+    catch(err){
+        console.log(err)
+    }
+});
+
+router.post("", async(req,res) => {
+    try {
+        const users = await User.create(req.body).lean().exec();
+    }
+    catch(err){
+        console.log(err)
+    }
+});
+
+
+module.exports = router
