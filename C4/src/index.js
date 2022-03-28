639b90c103a0103744b49cc6fb1968e3f0de5e32
Teju0910 <tejasvini_fw15_007@masai.school>
2022-03-28 11:34:26 +0530

    update

--- C4/src/index.js
+++ C4/src/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const connect = require("./configs/db");
+const UserController = require("./controllers/user.controller");
+const todoController = require("./controllers/todo.controller")
 
 const app = express();
+app.use(express.json())
+app.use("/user",UserController);
+app.use("/todo",todoController)
 
-app.use(express)
+app.listen(1010, async() => {
+    try{
+        console.log("listing to port 1010")
+        await connect();
+        console.log("listing to port 1010")
+    }
+    catch(err){
+        console.log(err)
+    }
+})
